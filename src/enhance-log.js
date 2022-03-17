3bfba650efd16879f9de96930838a2cc44de5d9c
luke-trimby <luke@lukasone.co.uk>
2022-03-17 10:05:51 +0000

    Enchance Log!

---
+++ src/enhance-log.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Log = void 0;
+class Log {
+    static i(message, ...vars) {
+        Log.o(Log.color.info, message, ...vars);
+    }
+    static d(message, ...vars) {
+        Log.o(Log.color.debug, message, ...vars);
+    }
+    static w(message, ...vars) {
+        Log.o(Log.color.warn, message, ...vars);
+    }
+    static c(color = Log.color.debug, message, ...vars) {
+        Log.o(color, message, ...vars);
+    }
+    static e(message, ...vars) {
+        Log.o(Log.color.error, message, ...vars);
+    }
+    static throw(errorCode, message) {
+        throw new Error(errorCode + ": " + message);
+    }
+    static o(color, message, ...vars) {
+        const date = new Date;
+        const dateFormated = ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2) + " " + ("000" + date.getMilliseconds()).slice(-4);
+        console.log("%c" + "[" + dateFormated + "] " + message, "color:" + color + "; font-weight:bold", ...vars);
+    }
+}
+exports.Log = Log;
+Log.color = {
+    info: '#EEEEEE',
+    debug: '#6666FF',
+    warn: '#FF6633',
+    error: '#FF3333'
+};
